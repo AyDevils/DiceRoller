63ff66955581a5c25bc7bfa60a1520f651859c7c
ayush rawat <ayushrawat@ayushs-MacBook-Pro.local>
2019-11-07 14:18:29 +0000

    Initila time

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.diceroller;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void RandomNo(View view)
+    {
+      Random Ran = new Random();
+      int number = Ran.nextInt(6)+1;
+      TextView mrtext = (TextView) findViewById(R.id.textView);
+      String myString = String.valueOf(number);
+        mrtext.setText(myString);
+
+        EditText myText;
+        myText = (EditText) findViewById(R.id.editText);
+        int numberinput= Integer.parseInt(myText.getText().toString());
+
+        if (numberinput < 1 || numberinput >6)
+        {
+            Toast.makeText(this, " Wrong! Try again b", Toast.LENGTH_SHORT).show();
+        }
+        else if (numberinput == number)
+        {
+            Toast.makeText(this, " Congratulation", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
+}
